62c0031e369f91a049d91a1b70d3c6654ea88ee2
DypenCrest <dashadipen@gmail.com>
2024-11-08 14:21:51 +0545

    title fix

--- src/assets/data/headerData.js
+++ src/assets/data/headerData.js
@@ -3,7 +3,7 @@ import dipenImg from "../images/hero/dipen.jpg";
 
 export const headerData = {
   name: "Dipen Shrestha",
-  title: "Web Developer",
+  title: "Frontend Developer",
   description:
     "There wasn't a sound in the forest, but it wasn't the silence that concerned him. It was the mist. The thick, rolling fog that shouldn't be here, but it was. He knew what it meant and he was ready.",
   image: dipenImg,
