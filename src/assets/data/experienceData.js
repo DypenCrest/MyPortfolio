b9c9fcd33d20cac37b74e158eb8f41e2524f1e96
Dipen Shrestha <65706276+DypenCrest@users.noreply.github.com>
2024-12-12 11:03:54 +0545

    latest

--- src/assets/data/experienceData.js
+++ src/assets/data/experienceData.js
@@ -1,14 +1,22 @@
 export const experienceData = [
   {
     id: 1,
-    company: "",
-    jobtitle: "Self Learning Frontend Development",
-    desc: "Took L2 Diploma Course at Skillsikshya of Frontend. Building beautiful responsive designs and learning reactJs, MUI, tailwind Css, redux/Zustand, React Hook Form, axios, React Query.",
+    company: "Vrit Technologies",
+    jobtitle: "Frontend Intern",
+    desc: "Team up with vrit technologies team to Learn and build beautiful responsive web applications. ",
     startYear: "2024",
     endYear: "Present",
   },
   {
     id: 2,
+    company: "",
+    jobtitle: "Self Learning Frontend Development",
+    desc: "Took L2 Diploma Course at Skillsikshya of Frontend. Building beautiful responsive designs and learning reactJs, MUI, tailwind Css, redux/Zustand, React Hook Form, axios, React Query.",
+    startYear: "2024",
+    endYear: "2024",
+  },
+  {
+    id: 3,
     company: "Kalash Services",
     jobtitle: "Websurfer ITSR",
     desc: "Monitor Down links, Connect with clients and troubleshoot any problems using different tools like nagios,podnms, zabbix, etc. Collaborate with Senior team and all the other departments to troubleshoot. Manage Data of connected client's link in database.",
